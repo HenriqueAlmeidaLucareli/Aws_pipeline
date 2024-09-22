8993e480194ec45732a1fedcfce54e9445041e2e
HenriqueAlmeidaLucareli <henrique.lucareli@germinare.org.br>
2024-09-22 09:50:29 -0300

    v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<center><h1><font color=red>Oi, nova atualização via pipeline, ajuste feito</font></h1></center> '
+    return '<center><h1><font color=red>Oi, nova atualização via pipeline, ajuste feito, ajuste feito para a aws e o  render ao mesmo tempo </font></h1></center> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
