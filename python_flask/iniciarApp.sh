e840d790c84c2089255754231a6c4d5106437c43
HenriqueAlmeidaLucareli <henrique.lucareli@germinare.org.br>
2024-09-26 19:44:30 -0300

    v4

--- python_flask/iniciarApp.sh
+++ python_flask/iniciarApp.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-# Ativar o ambiente virtual
-source ~/Aws_pipeline/my_env/bin/activate
-
-# Iniciar o Gunicorn
-exec gunicorn --workers 3 --bind unix:/home/ubuntu/Aws_pipeline/app.sock app:app
+source my_env/bin/activate
+python3 app.py
